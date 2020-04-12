4f80938e6aa0091e762321f35cfc8fa0a79d2a2f
Rachel Levy <57646446+rwillowlevy@users.noreply.github.com>
2020-04-12 17:58:54 -0500

    Update app.js
    initial section to run

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-
+const inquirer = require("inquirer");
+const jest = require("jest");
+const Engineer = require("./lib/engineer.js");
+const Intern = require("./lib/intern.js");
+const Manager = require("./lib/manager.js");
+var uniqueId = 0;
+var teamArray = [];
