d2bb38450e99e9db5d462d490a261533212af261
Rachel Levy <57646446+rwillowlevy@users.noreply.github.com>
2020-04-12 18:05:11 -0500

    Update app.js
    prompts depending on the role

--- app.js
+++ app.js
@@ -5,3 +5,87 @@ const Intern = require("./lib/intern.js");
 const Manager = require("./lib/manager.js");
 var uniqueId = 0;
 var teamArray = [];
+
+function promptUser(answers) {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "role",
+            message: "what is your role?",
+            choices: ["Engineer", "Intern", "Manager"]
+        },
+    ]).then(function (res) {
+        if (res.role === "Engineer") {
+            inquirer.prompt([
+                {
+                    name: "name",
+                    message: "What is your name?",
+                    type: "input"
+                },
+                {
+                    name: "github",
+                    type: "input",
+                    message: "What is your github Username?"
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is your email?"
+                }
+            ]).then(function (engineerRes) {
+                var newEngineer = new Engineer(engineerRes.name, engineerRes.email, uniqueId, engineerRes.github);
+                uniqueId = uniqueId + 1; 
+                teamArray.push(newEngineer);
+                addUser();
+                
+            });
+
+        } else if (res.role === "Intern") {
+            inquirer.prompt([
+                {
+                    name: "name",
+                    message: "What is your name?",
+                    type: "input"
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is your email?"
+                },
+                {
+                    name: "school",
+                    type: "input",
+                    message: "Where did you graduate from college?"
+                }
+            ]).then(function (internRes) {
+                var newIntern = new Intern(internRes.name, internRes.email, uniqueId, internRes.school);
+                uniqueId = uniqueId + 1; 
+                teamArray.push(newIntern);
+                addUser();
+            });
+        } else if (res.role === "Manager") {
+            inquirer.prompt([
+                {
+                    name: "name",
+                    message: "What is your name?",
+                    type: "input"
+                },
+                {
+                    name: "email",
+                    type: "input",
+                    message: "What is your email?"
+                },
+                {
+                    name: "office",
+                    type: "input",
+                    message: "What is your office number?"
+                }
+            ]).then(function (managerRes) {
+                var newManager = new Manager(managerRes.name, managerRes.email, uniqueId, managerRes.office);
+                uniqueId = uniqueId + 1; 
+                teamArray.push(newManager);
+                addUser();
+            });
+        };
+
+    })
