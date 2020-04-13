813b244c746e52cf725800112a72f2395526b65b
Rachel Levy <57646446+rwillowlevy@users.noreply.github.com>
2020-04-12 19:08:52 -0500

    Update app.js
    A way simpler approach to the app.js. Wanted to clean it up and condense it.

--- app.js
+++ app.js
@@ -1,120 +1,35 @@
 const inquirer = require("inquirer");
-const jest = require("jest");
-const Engineer = require("./lib/engineer.js");
-const Intern = require("./lib/intern.js");
-const Manager = require("./lib/manager.js");
-var uniqueId = 0;
-var teamArray = [];
+const fs = require("fs");const Employee = require("./lib/Employee");
 
-function promptUser(answers) {
+const Employee = require("./lib/Employee")
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+
+
+
+function promptUser() {
     return inquirer.prompt([
         {
-            type: "list",
+            type: "input",
+            message: "Enter your first name:",
+            name: "name"
+        }, {
+            type: "input",
+            message: "Enter your email address:",
+            name: "email"
+        }, {
+            type: "input",
+            message: "What's your role within the company?",
             name: "role",
-            message: "what is your role?",
-            choices: ["Engineer", "Intern", "Manager"]
+            choices: ['engineer', 'intern', 'manager']
+            //conditional statement about employment: intern ask for school, if manager ask for office number, if engineer ask for githubname
         },
-    ]).then(function (res) {
-        if (res.role === "Engineer") {
-            inquirer.prompt([
-                {
-                    name: "name",
-                    message: "What is your name?",
-                    type: "input"
-                },
-                {
-                    name: "github",
-                    type: "input",
-                    message: "What is your github Username?"
-                },
-                {
-                    name: "email",
-                    type: "input",
-                    message: "What is your email?"
-                }
-            ]).then(function (engineerRes) {
-                var newEngineer = new Engineer(engineerRes.name, engineerRes.email, uniqueId, engineerRes.github);
-                uniqueId = uniqueId + 1; 
-                teamArray.push(newEngineer);
-                addUser();
-                
-            });
-
-        } else if (res.role === "Intern") {
-            inquirer.prompt([
-                {
-                    name: "name",
-                    message: "What is your name?",
-                    type: "input"
-                },
-                {
-                    name: "email",
-                    type: "input",
-                    message: "What is your email?"
-                },
-                {
-                    name: "school",
-                    type: "input",
-                    message: "Where did you graduate from college?"
-                }
-            ]).then(function (internRes) {
-                var newIntern = new Intern(internRes.name, internRes.email, uniqueId, internRes.school);
-                uniqueId = uniqueId + 1; 
-                teamArray.push(newIntern);
-                addUser();
-            });
-        } else if (res.role === "Manager") {
-            inquirer.prompt([
-                {
-                    name: "name",
-                    message: "What is your name?",
-                    type: "input"
-                },
-                {
-                    name: "email",
-                    type: "input",
-                    message: "What is your email?"
-                },
-                {
-                    name: "office",
-                    type: "input",
-                    message: "What is your office number?"
-                }
-            ]).then(function (managerRes) {
-                var newManager = new Manager(managerRes.name, managerRes.email, uniqueId, managerRes.office);
-                uniqueId = uniqueId + 1; 
-                teamArray.push(newManager);
-                addUser();
-            });
-        };
-
-    })
-        .catch(function (err) {
-        });
-};
-
-function generateHTML() {
-    
-    function renderManager() {
-
-    };
-    function renderIntern() {
-
-    };
-    function renderEngineer() {
-
-    };
-};
-function addUser(){
-    inquirer.prompt([
-        {   
-            name: "continue",
-            message: "Do you want to add another team member?",
-            type: "confirm"
-        }
-    ]).then(function(confirmRes){
-        confirmRes.continue ? promptUser() : generateHTML()
-    })
-};
-promptUser();
+        {
+            type: "input",
+            message: "Enter your github username:",
+            name: "username"
+        }, ]);
+}
 
